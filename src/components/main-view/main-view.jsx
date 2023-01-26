3d2395b32993672ece2af93daf585fce776fa8c7
maconrgarilao <108959350+maconrgarilao@users.noreply.github.com>
2023-01-25 21:27:41 -0500

    Render Data in Components

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,33 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-
-        {   id: 1,
-            Title: "Notting Hill",
-            Description: "The story is of a romance between a London bookseller (Grant) and a famous American actress (Roberts) who happens to walk into his shop.",
-            Image: "https://m.media-amazon.com/images/M/MV5BMTE5OTkwYzYtNDhlNC00MzljLTk1YTktY2IxZjliZmNjMjUzL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg",
-            Director: "Roger Michell" 
-        },
-        {
-            id: 2,
-            Title: "300",
-            Description: "The plot revolves around King Leonidas, who leads 300 Spartans into battle against Persian god-king Xerxes and his big and good army of more than 300,000 soldiers.",
-            Image: "https://upload.wikimedia.org/wikipedia/en/5/5c/300poster.jpg",
-            Director: "Zack Snyder"
-        },
-        {
-            id: 3,
-            Title: "Titanic",
-            Description: "Incorporating both historical and fictionalized aspects, it is based on accounts of the sinking of the RMS Titanic and stars Leonardo DiCaprio and Kate Winslet as members of different social classes who fall in love aboard the ship during its ill-fated maiden voyage.",
-            Image: "https://upload.wikimedia.org/wikipedia/en/1/18/Titanic_%281997_film%29_poster.png",
-            Director: "James Cameron"
-        }
-    ]);
+    const [movies, setMovies] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect(() => {
+        fetch("")
+            .then((response) => response.json())
+            .then((data) => {
+                const moviesFromApi = data.docs.map((doc) => {
+                    return {
+                        id: doc.key,
+                        title: doc.title,
+                        image: src.image,
+                        director: doc.director
+                    };
+                });
+
+                setMovies(moviesfromApi);
+            });
+    }, []);
 
     if (selectedMovie) {
         return (
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,22 +1,43 @@
-export const MovieView = ({ movie, onBackClick }) => {
-    return (
-        <div>
-            <div>
-                <img src={movie.Image} />
+import React from "react";
+import PropTypes from "prop-types";
+
+export class MovieView extends React.Component {
+
+    render() {
+        const { movie, onBackClick } = this.props;
+
+        return (
+            <div className="movie-view">
+                <div className="movie-poster">
+                    <img src={movie.ImagePath} />
+                </div>
+                <div className="movie-title">
+                    <span className="label">Title: </span>
+                    <span className="value">{movie.Title}</span>
+                </div>
+                <div className="movie-description">
+                    <span className="label">Description: </span>
+                    <span className="value">{movie.Description}</span>
+                </div>
+                <button onClick={() => { onBackClick(null); }}>Back</button>
             </div>
-            <div>
-                <span>Title: </span>
-                <span>{movie.Title}</span>
-            </div>
-            <div>
-                <span>Description: </span>
-                <span>{movie.Description}</span>
-            </div>
-            <div>
-                <span>Director: </span>
-                <span>{movie.Director}</span>
-            </div>
-            <button onClick={onBackClick}>Back</button>
-        </div>
-    );
+        );
+    }
+}
+
+MovieView.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        ImagePath: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired,
+        }).isRequired,
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Bio: PropTypes.string.isRequired,
+        }).isRequired,
+    }).isRequired,
+    onBackClick: PropTypes.func.isRequired,
 };
