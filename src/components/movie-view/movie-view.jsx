3ee687c74ad94d469150bf2d51b6d5021a4d7c95
maconrgarilao <108959350+maconrgarilao@users.noreply.github.com>
2023-02-03 21:32:28 -0500

    Created NavigationBar and Routing

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,44 +1,30 @@
 import "./movie-view.scss";
 import React from "react";
-import PropTypes from "prop-types";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
-export class MovieView extends React.Component {
+export const MovieView = ({ movies }) => {
+    const { index } = useParams();
 
-    render() {
-        const { movie, onBackClick } = this.props;
+    const movie = movies.find((m) => m.id === index);
 
-        return (
-            <div className="movie-view">
-                <div className="movie-poster">
-                    <img src={movie.ImagePath} />
-                </div>
-                <div className="movie-title">
-                    <span className="label">Title: </span>
-                    <span className="value">{movie.Title}</span>
-                </div>
-                <div className="movie-description">
-                    <span className="label">Description: </span>
-                    <span className="value">{movie.Description}</span>
-                </div>
-                <button onClick={onBackClick} className="back-button" style={{ cursor: "pointer" }}>Back</button>
+    return (
+        <div className="movie-view">
+            <div className="movie-poster">
+                <img className="w-100" src={movie.ImagePath} />
             </div>
-        );
-    }
-}
-
-MovieView.propTypes = {
-    movie: PropTypes.shape({
-        Title: PropTypes.string.isRequired,
-        Description: PropTypes.string.isRequired,
-        ImagePath: PropTypes.string.isRequired,
-        Genre: PropTypes.shape({
-            Name: PropTypes.string.isRequired,
-            Definition: PropTypes.string.isRequired,
-        }).isRequired,
-        Director: PropTypes.shape({
-            Name: PropTypes.string.isRequired,
-            Bio: PropTypes.string.isRequired,
-        }).isRequired,
-    }).isRequired,
-    onBackClick: PropTypes.func.isRequired,
+            <div className="movie-title">
+                <span>Title: </span>
+                <span>{movie.Title}</span>
+            </div>
+            <div className="movie-director">
+                <span>Director: </span>
+                <span>{movie.Director.Name}</span>
+            </div>
+            <Link to={`/`}>
+                <button className="back-button" style={{ cursor: "pointer" }}>Back</button>
+            </Link>
+        </div>
+    );
 };
+
