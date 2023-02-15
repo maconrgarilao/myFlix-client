b61f2fce54768248396a19a92a0e4e524f6450d9
maconrgarilao <108959350+maconrgarilao@users.noreply.github.com>
2023-02-15 12:24:12 -0500

    Installed React-Redux and Redux Toolkit

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import { MainView } from "./components/main-view/main-view";
 import Container from "react-bootstrap/Container";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
 //import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
+import { Provider } from "react-redux";
 
 //main component (will eventually use all the others)
 const MyFlixApplication = () => {
     return (
+        <Provider store={store}>
         <Container>
             <MainView />
         </Container>
+        </Provider>
     );
 };
 
